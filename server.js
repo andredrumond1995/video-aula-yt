cd362af199c0a7165147bd95dc860baad9edf422
Andre Drumond das Chagas <andre.drumond@dev.pro>
2023-10-10 22:16:24 +0000

    style(users): formatação da função de criar usuários

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 function criarUsuario(body) {
-    //lógica para criação do usuário no banco de dados
-    return { nome: body.name, id: 1 };
+  //lógica para criação do usuário no banco de dados
+  return { nome: body.name, id: 1 };
 }
 
 app.post('/users', (req, res) => {
