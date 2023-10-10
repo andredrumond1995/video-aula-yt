27b74424f2392f78b833c5de8fa9d8dde56d775f
Andre Drumond das Chagas <andre.drumond@dev.pro>
2023-10-10 22:02:53 +0000

    feat(users): adiciona rota para criar usuários

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.post('/users', (req, res) => {
+  // Lógica para criar usuário
+  // ...
+  res.json({ message: 'Usuário criado com sucesso' });
+});
+
 app.listen(port, () => {
   console.log(`Servidor está rodando`);
 });
